da0709c3619d08ea518f78ef344d39acfda88536
Subtext Pro <hello@subtext.pro>
2023-01-02 23:36:46 -0500

    commit tree 92937a81655c859f36a3b050c63a1b783b6bbabb

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarf= ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves do |dwarf|
+  dwarf.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet do |planeteer_calls|
+    planeteer_calls.map do |call|
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls do |planeteer_calls|
+  planeteer_calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese do |snacks|
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do |maybe_cheese|
+    cheese_types.include?(maybe_cheese)
+  end
 end
